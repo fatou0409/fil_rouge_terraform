796052b018e53feb181e3a302977ba46b422d0bb
fatou0409 <fatou97diarra@gmail.com>
2025-05-13 09:24:02 +0000

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
 output "backend_container_name" {
   description = "Nom du conteneur backend"
-  value       = docker_container.backend_app.name
+  value       = docker_container.backend_app_new.name
 }
 
 output "frontend_container_name" {
   description = "Nom du conteneur frontend"
-  value       = docker_container.frontend_app.name
+  value       = docker_container.frontend_app_new.name
 }
 
 output "backend_port" {
   description = "Port du backend exposé"
-  value       = docker_container.backend_app.ports[0].external
+  value       = docker_container.backend_app_new.ports[0].external
 }
 
 output "frontend_port" {
   description = "Port du frontend exposé"
-  value       = docker_container.frontend_app.ports[0].external
+  value       = docker_container.frontend_app_new.ports[0].external
 }

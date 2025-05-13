29e88d0df4c82d52d76cf49b39c4890d10cf5e4b
fatou0409 <fatou97diarra@gmail.com>
2025-05-13 09:15:26 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "docker_user" {
   description = "Nom d'utilisateur Docker Hub"
   type        = string
-  default     = "fatou0409"
+  default     = "fatou0409"  # Remplace si nécessaire par ton nom d'utilisateur Docker Hub
 }

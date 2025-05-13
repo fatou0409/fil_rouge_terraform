1d9292b175f2353b5cd465e1ab417e3c6b91743a
fatou0409 <fatou97diarra@gmail.com>
2025-05-13 08:57:45 +0000

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,19 @@
-output "backend_container_ip" {
-  value = docker_container.backend_app.ip_address
+output "backend_container_name" {
+  description = "Nom du conteneur backend"
+  value       = docker_container.backend_app.name
 }
 
-output "frontend_container_ip" {
-  value = docker_container.frontend_app.ip_address
+output "frontend_container_name" {
+  description = "Nom du conteneur frontend"
+  value       = docker_container.frontend_app.name
+}
+
+output "backend_port" {
+  description = "Port du backend exposé"
+  value       = docker_container.backend_app.ports[0].external
+}
+
+output "frontend_port" {
+  description = "Port du frontend exposé"
+  value       = docker_container.frontend_app.ports[0].external
 }

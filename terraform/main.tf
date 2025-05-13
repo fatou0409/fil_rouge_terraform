d8cba9ac61d4761351e4d1de00343a9af9549a37
fatou0409 <fatou97diarra@gmail.com>
2025-05-13 08:52:33 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,56 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+  host = "npipe:////./pipe/docker_engine" # Utilisé sous Windows avec Docker Desktop
+}
+
+variable "docker_user" {
+  description = "Nom d'utilisateur Docker Hub"
+  type        = string
+  default     = "fatou0409"
 }
 
 resource "docker_image" "backend_image" {
   name = "${var.docker_user}/profilapp-backend:latest"
   build {
-    context = "./Backend-main/odc"
+    context = "${path.module}/../Backend-main/odc"
   }
 }
 
 resource "docker_image" "frontend_image" {
   name = "${var.docker_user}/profilapp-frontend:latest"
   build {
-    context = "./Frontend-main"
+    context = "${path.module}/../Frontend-main"
   }
 }
 
 resource "docker_container" "backend_app" {
   name  = "backend_app"
-  image = docker_image.backend_image.latest
+  image = docker_image.backend_image.name
+
   ports {
     internal = 8080
     external = 8080
   }
+
+  depends_on = [docker_image.backend_image]
 }
 
 resource "docker_container" "frontend_app" {
   name  = "frontend_app"
-  image = docker_image.frontend_image.latest
+  image = docker_image.frontend_image.name
+
   ports {
     internal = 80
     external = 80
   }
+
+  depends_on = [docker_image.frontend_image]
 }
